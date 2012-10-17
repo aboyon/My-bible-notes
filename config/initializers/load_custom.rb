bae62f8f70e22833bc0d3ed279d7cc4c3e4d73cd
David Silveira <jdsilveira@gmail.com>
2012-10-17 17:00:57 -0300

    load_custom.rb reverted. Config files moved again under config/ folder

--- config/initializers/load_custom.rb
+++ config/initializers/load_custom.rb
@@ -4,8 +4,4 @@ require 'xmlsimple'
 require 'typhoeus'
 
 # getting configuration file
-if Rails.env.production?
-	APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[ENV["RAILS_ENV"]]
-else
-	APP_CONFIG = YAML.load_file("#{Rails.root.join('app').to_s}/config/config.yml")[ENV["RAILS_ENV"]]
-end
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[ENV["RAILS_ENV"]]
