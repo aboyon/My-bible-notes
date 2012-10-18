fe586d14c3f2c54bdf2e12771451ce143f4d4112
David Silveira <jdsilveira@gmail.com>
2012-10-18 16:29:15 -0300

    load_custom.rb file changed. YAML.load is now using an ERB.new object inside in order to execute ruby syntax inside

--- config/initializers/load_custom.rb
+++ config/initializers/load_custom.rb
@@ -4,4 +4,4 @@ require 'xmlsimple'
 require 'typhoeus'
 
 # getting configuration file
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[ENV["RAILS_ENV"]]
+APP_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/config.yml")).result)[Rails.env]
